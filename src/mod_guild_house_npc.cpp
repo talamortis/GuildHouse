56ade1f0b89232d678419a15872674b362f76e8f
Rob <rob@rbedfordpro.com>
2019-10-08 19:08:23 -0700

    removing comments

--- src/mod_guild_house_npc.cpp
+++ src/mod_guild_house_npc.cpp
@@ -26,11 +26,9 @@ public:
 
         if (player->GetGuild())
         {
-            // WIP - Anhanga
             Guild* guild = sGuildMgr->GetGuildById(player->GetGuildId());
             Guild::Member const* memberMe = guild->GetMember(player->GetGUID());
             if ((int)memberMe->GetRankId() > GuildHouseBuyRank)
-            //if (player->GetGuild()->GetLeaderGUID() != player->GetGUID())
             {
                 ChatHandler(player->GetSession()).PSendSysMessage("You are not authorized to make guild house purchases (RankId: %hhu | %d).", memberMe->GetRankId(), GuildHouseBuyRank);
                 return false;
